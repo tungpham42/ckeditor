655178c7576793d760de4e3cd092c05210b9cd1e
Tung Pham <tung.42@gmail.com>
2022-01-11 19:43:01 +0700

    some changes

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -22,8 +22,6 @@ import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
 import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
-import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import HtmlComment from '@ckeditor/ckeditor5-html-support/src/htmlcomment.js';
 import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
@@ -42,20 +40,10 @@ import List from '@ckeditor/ckeditor5-list/src/list.js';
 import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
-import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
-import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
-import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
-import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
-import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
-import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin.js';
-import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js';
-import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
@@ -75,20 +63,13 @@ Editor.builtinPlugins = [
 	Autosave,
 	BlockQuote,
 	Bold,
-	Code,
-	CodeBlock,
-	DataFilter,
-	DataSchema,
 	Essentials,
-	FindAndReplace,
 	FontBackgroundColor,
 	FontColor,
 	FontFamily,
 	FontSize,
 	GeneralHtmlSupport,
 	Heading,
-	Highlight,
-	HorizontalLine,
 	HtmlComment,
 	HtmlEmbed,
 	Image,
@@ -107,20 +88,10 @@ Editor.builtinPlugins = [
 	ListStyle,
 	MediaEmbed,
 	MediaEmbedToolbar,
-	PageBreak,
 	Paragraph,
 	PasteFromOffice,
 	RemoveFormat,
-	SpecialCharacters,
-	SpecialCharactersArrows,
-	SpecialCharactersCurrency,
-	SpecialCharactersEssentials,
-	SpecialCharactersLatin,
-	SpecialCharactersMathematical,
-	SpecialCharactersText,
 	Strikethrough,
-	Subscript,
-	Superscript,
 	Table,
 	TableCaption,
 	TableCellProperties,
@@ -134,10 +105,7 @@ Editor.builtinPlugins = [
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'removeFormat',
-			'|',
 			'heading',
-			'|',
 			'fontSize',
 			'fontFamily',
 			'fontColor',
@@ -147,8 +115,6 @@ Editor.defaultConfig = {
 			'italic',
 			'underline',
 			'strikethrough',
-			'subscript',
-			'superscript',
 			'|',
 			'alignment',
 			'|',
@@ -163,14 +129,12 @@ Editor.defaultConfig = {
 			'imageInsert',
 			'insertTable',
 			'mediaEmbed',
-			'|',
 			'htmlEmbed',
-			'findAndReplace',
-			'specialCharacters',
-			'horizontalLine',
 			'|',
 			'undo',
-			'redo'
+			'redo',
+			'|',
+			'removeFormat',
 		]
 	},
 	language: 'en',
